cc329b6755d8f3422e79ae18c4df354f2fc02fe3
Arthur Neves <arthurnn@gmail.com>
2015-12-08 16:08:29 -0500

    add ping route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  match "/ping" => lambda{ |env| [200, {'Content-Type' => 'text/plain'}, ['PONG']] }
+
   mount Resque::Server.new, at: '/resque'
   mount Shipit::Engine, at: '/'
 end
