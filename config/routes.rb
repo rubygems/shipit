a24b9fa87a5c350252d640b8d1586dcc8c7c5cdc
Arthur Neves <arthurnn@gmail.com>
2015-12-08 16:20:17 -0500

    fix ping route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  match "/ping" => lambda{ |env| [200, {'Content-Type' => 'text/plain'}, ['PONG']] }
+  get "/ping" => lambda{ |env| [200, {'Content-Type' => 'text/plain'}, ['PONG']] }
 
   mount Resque::Server.new, at: '/resque'
   mount Shipit::Engine, at: '/'
