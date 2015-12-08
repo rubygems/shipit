3f3a195c814358fb87a8e970ce213e93f48eadb5
Arthur Neves <arthurnn@gmail.com>
2015-12-08 16:50:03 -0500

    remove ping route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get "/ping" => lambda{ |env| [200, {'Content-Type' => 'text/plain'}, ['PONG']] }
-
   mount Resque::Server.new, at: '/resque'
   mount Shipit::Engine, at: '/'
 end
